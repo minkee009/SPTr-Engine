ecc66de1f0088788298d5d8b47374ea755e218f2
minkee009 <minkee5004@gmail.com>
2024-02-22 12:21:59 +0900

    24-02-22
    인풋 비트플래그 재설정

--- SPTrEngine/Input.cs
+++ SPTrEngine/Input.cs
@@ -7,23 +7,24 @@ namespace SPTrEngine
         /// <summary>
         /// 현재 입력의 비트마스크
         /// </summary>
-        public static ulong[] CurrentInput => _currentInput;
+        public static uint[] CurrentInput => _currentInput;
 
         /// <summary>
         /// 과거 입력의 비트마스크
         /// </summary>
-        public static ulong[] OldInput => _oldInput;
+        public static uint[] OldInput => _oldInput;
 
-        static ulong[] _currentInput = new ulong[4];
-        static ulong[] _oldInput = new ulong[4];
+        static uint[] _currentInput = new uint[8];
+        static uint[] _oldInput = new uint[8];
 
         /// <summary>
         /// WinAPI - user32.dll용 상수, 키가 눌러져 있는 경우
         /// </summary>
         const int KEY_PRESSED = 0x8000;
+        const uint UNSINGED_ONE = 1;
 
         /// <summary>
-        /// 매개변수로 전달한 키값의 상태를 확인합니다.
+        /// 매개변수로 전달받은 키값의 상태를 확인합니다.
         /// </summary>
         /// <param name="vKey">키의 고유번호</param>
         /// <returns>키의 상태</returns>
@@ -32,22 +33,22 @@ namespace SPTrEngine
 
         public static bool GetKey(ConsoleKey keyCode)
         {
-            var i = (int)keyCode / 64;
-            return (_currentInput[i] & ((ulong)1 << (int)keyCode)) != 0;
+            var i = (int)keyCode / 32;
+            return (_currentInput[i] & (UNSINGED_ONE << (int)keyCode) % 32) != 0;
         }
 
         public static bool GetKeyUp(ConsoleKey keyCode)
         {
-            var i = (int)keyCode / 64;
-            return (_oldInput[i] & ((ulong)1 << (int)keyCode)) != 0
-                && (_currentInput[i] & ((ulong)1 << (int)keyCode)) == 0;
+            var i = (int)keyCode / 32;
+            return (_oldInput[i] & (UNSINGED_ONE << (int)keyCode) % 32) != 0
+                && (_currentInput[i] & (UNSINGED_ONE << (int)keyCode) % 32) == 0;
         }
 
         public static bool GetKeyDown(ConsoleKey keyCode)
         {
-            var i = (int)keyCode / 64;
-            return (_oldInput[i] & ((ulong)1 << (int)keyCode)) == 0
-                && (_currentInput[i] & ((ulong)1 << (int)keyCode)) != 0;
+            var i = (int)keyCode / 32;
+            return (_oldInput[i] & (UNSINGED_ONE << (int)keyCode) % 32) == 0
+                && (_currentInput[i] & (UNSINGED_ONE << (int)keyCode) % 32) != 0;
         }
 
         /// <summary>
@@ -55,23 +56,23 @@ namespace SPTrEngine
         /// </summary>
         public static void SetInput()
         {
-            ulong[] nInput = { 0,0,0,0 };
+            uint[] nInput = { 0,0,0,0,0,0,0,0 };
 
             foreach(ConsoleKey key in Enum.GetValues(typeof(ConsoleKey)))
             {
                 if((GetAsyncKeyState((int)key) & KEY_PRESSED) != 0)
                 {
-                    var i = (int)key / 64;
-                    nInput[i] |= (ulong)1 << (int)key % 64;
+                    var i = (int)key / 32;
+                    nInput[i] |= (uint)1 << (int)key % 32;
                 }
             }
 
-            //foreach(var input in nInput)
-            //{
-            //    Console.Write($" {Convert.ToString((long)input, toBase: 2)}");
-            //}
+            foreach (var input in nInput)
+            {
+                Console.Write($" {Convert.ToString(input, toBase: 2)}");
+            }
 
-            //Console.WriteLine();
+            Console.WriteLine();
 
             _oldInput = _currentInput;
             _currentInput = nInput;
