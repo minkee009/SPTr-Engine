cd33ceabbf7f95b087718a7b74bffea30ba9b0e0
minkee009 <minkee5004@gmail.com>
2024-04-07 20:43:37 +0900

    24-04-07
    - winform 기본세팅

--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -1,25 +1,31 @@
-﻿using SPTrEngine;
+﻿using SPTrApp.ExampleApp;
+using SPTrEngine;
 using SPTrEngine.Math.Vector;
 
 namespace SPTrApp
 {
     internal static class Program
     {
+        [STAThread]
         internal static void Main(string[] args)
         {
-            InternalCMD.MainArgsCMD(args);
+            ApplicationConfiguration.Initialize();
 
-            Enemy enemy = new Enemy('E');
-            enemy.name += " | enemy";
-            enemy.position = new Vector2 { x = 3, y = 7 };
+            Application.Run(new Form1());
 
-            Player player = new Player('P');
-            player.name += " | player";
-            player.position = new Vector2 { x = 5, y = 4 };
+            //InternalCMD.MainArgsCMD(args);
 
-            BaseEngine.instance.SetScreenSize(24, 24);
+            //Enemy enemy = new Enemy('E');
+            //enemy.name += " | enemy";
+            //enemy.position = new Vector2 { x = 3, y = 7 };
 
-            BaseEngine.instance.Run();
+            //Player player = new Player('P');
+            //player.name += " | player";
+            //player.position = new Vector2 { x = 5, y = 4 };
+
+            //BaseEngine.instance.SetScreenSize(24, 24);
+
+            //BaseEngine.instance.Run();
         }
     }
 }
