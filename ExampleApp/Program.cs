10fd2d6f7c4f7ae3fb4cddb329a5e3101b3a6874
minkee009 <minkee5004@gmail.com>
2024-05-08 14:18:09 +0900

    24-05-08_test01
    - 실험코드 작성

--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -1,5 +1,8 @@
-﻿using SPTrEngine;
+﻿using SFML.Graphics;
+using SPTrEngine;
 using SPTrEngine.Math.Vector;
+using SFML.Window;
+using SFML;
 
 namespace SPTrApp
 {
@@ -8,19 +11,44 @@ namespace SPTrApp
         [STAThread]
         internal static void Main(string[] args)
         {
-            InternalCMD.MainArgsCMD(args);
+            RenderWindow window = new RenderWindow(new VideoMode(320, 240), "엿이나 잡숴!");
+            window.Closed += OnClose;
 
-            Enemy enemy = new Enemy('E');
-            enemy.name += " | enemy";
-            enemy.position = new Vector2 { x = 3, y = 7 };
+            Color BGColor = new Color(0, 192, 255);
 
-            Player player = new Player('P');
-            player.name += " | player";
-            player.position = new Vector2 { x = 5, y = 4 };
+            while(window.IsOpen)
+            {
+                //이벤트 실행
+                window.DispatchEvents();
 
-            BaseEngine.instance.EngineScreen.SetScreenSize(24,24);
+                //내부 화면 초기화
+                window.Clear(BGColor);
 
-            BaseEngine.instance.Run();
+                //윈도우 화면 출력
+                window.Display();
+            }
+
+            //InternalCMD.MainArgsCMD(args);
+
+            //Enemy enemy = new Enemy('E');
+            //enemy.name += " | enemy";
+            //enemy.position = new Vector2 { x = 3, y = 7 };
+
+            //Player player = new Player('P');
+            //player.name += " | player";
+            //player.position = new Vector2 { x = 5, y = 4 };
+
+            //BaseEngine.instance.EngineScreen.SetScreenSize(24,24);
+
+            //BaseEngine.instance.Run();
+        }
+
+        public static void OnClose(object? sender, EventArgs e)
+        {
+            if(sender as RenderWindow != null)
+            {
+                ((RenderWindow)sender).Close();
+            }
         }
     }
 }
